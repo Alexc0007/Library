db9c0ed88f85dc6f0e79e3dcf2050aa1feea3061
Alexc007 <Alexc007@DESKTOP-QT1OU0G>
2016-07-21 19:46:42 +0300

    added rb-delete and insertFixup methods

--- src/StudentManager.java
+++ src/StudentManager.java
@@ -66,7 +66,108 @@ public class StudentManager
 	//fix-up after insert method
 	public void rbInsertFixup(RBNode root , RBNode newStudent)
 	{
-		
+		RBNode temp; // this is "y"
+		while(newStudent.parent.color == RED)
+		{
+			if(newStudent.parent == newStudent.parent.parent.left) //if(p[z] = left[p[p[z]]] - parent is a left son
+			{
+				temp = newStudent.parent.parent.right; //y <- left[p[p[z]]]
+				if(temp.color == RED)
+				{
+					newStudent.parent.color = BLACK;        //case1
+					temp.color = BLACK;                     //case1
+					newStudent.parent.parent.color = RED;   //case1
+					newStudent = newStudent.parent.parent;  //case1
+				}
+				else
+				{
+					if(newStudent == newStudent.parent.right) //newStudent is right son
+					{
+						newStudent = newStudent.parent;    //case2
+						leftRotate(root , newStudent);     //case2
+					}
+					newStudent.parent.color = BLACK;       //case3
+					newStudent.parent.parent.color = RED;  //case3
+					rightRotate(root , newStudent.parent.parent);  //case3
+				}
+			}
+			else
+			{
+				temp = newStudent.parent.parent.left;
+				if(temp.color == RED)
+				{
+					newStudent.parent = BLACK;              //case4
+					temp.color = BLACK;                     //case4
+					newStudent.parent.parent.color = RED;   //case4
+					newStudent = newStudent.parent.parent;  //case4
+				}
+				else
+				{
+					if(newStudent == newStudent.parent.left) //newStudent is left son
+					{
+						newStudent = newStudent.parent;     //case5
+						rightRotate(root , newStudent);     //case5
+					}
+					newStudent.parent.color = BLACK;
+					newStudent.parent.parent.color = RED;
+					leftRotate(root , newStudent.parent.parent);
+				}
+				
+			}
+		}
+		root.color = BLACK;
 	}
 	
+	
+	//rb-delete method
+	public RBNode delete(RBNode root, RBNode studentRecord)
+	{
+		RBNode tempY;
+		RBNode tempX;
+		if(studentRecord.left == nullNode || studentRecord.right == nullNode) //means there is 1 son to the node we want to delete
+		{
+			tempY = studentRecord;
+		}
+		else 
+			tempY = treeSuccessor(node);
+		
+		if(tempY.left != nullNode)
+		{
+			tempX = tempY.left;
+		}
+		else
+		{
+			tempX = tempY.right;
+		}
+		tempX.parent = tempY.parent;
+		if(tempY.parent == nullNode)
+		{
+			root = tempX;
+		}
+		else
+		{
+			if(tempY == tempY.parent.left)
+			{
+				tempY.parent.left = tempX;
+			}
+			else
+			{
+				tempY.parent.right = tempX;
+			}
+		}
+		if(tempY.student.getId() != studentRecord.student.getId())
+		{
+			studentRecord.student.setID(tempY.student.getId());
+		}
+		if(tempY.color == BLACK)
+		{
+			deleteFixup(root , tempX);
+		}
+		return tempY;
+	}
+	//leftRotate method
+	public void leftRotate(RBNode root , RBNode newStudent)
+	{
+		
+	}
 }
